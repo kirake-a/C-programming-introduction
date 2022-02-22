dc93587d6a0285007180f3d0a353c01786bbc7a5
kirake-a <ruben_aalvarado@outlook.com>
2022-02-22 15:13:42 -0600

    Some new functions for the values with 2 and 3 digits

--- EjercicioClase/Modularidad/NumerosAPalabras.c
+++ EjercicioClase/Modularidad/NumerosAPalabras.c
@@ -4,12 +4,15 @@ resultado de la ejecucion imprima la cantidad de centenas, decenas y unidades */
 #include <stdio.h>
 
 //Prototiping all the functions
-void numeroAPAlabras();
+    //This is for the output
+void numeroAPAlabras(int centenas, int decenas, int unidades);
 
+    //This is to calculate the values for numbers with 3 digits...
 int valor3Digitos_centenas(int value);
 int valor3Digitos_decenas(int value);
 int valor3Digitos_unidades(int value);
 
+    //This is to calculate the values for numbers with 2 digits...
 int valor2Digitos_decenas(int value);
 int valor2Digitos_unidades(int value);
 
@@ -24,24 +27,25 @@ int main(){
         centenas = valor3Digitos_centenas(num);
         decenas = valor3Digitos_decenas(num);
         unidades = valor3Digitos_unidades(decenas);
-        void numeroAPalabras();
+        void numeroAPalabras(centenas, decenas, unidades);
     }
     else if(num >= 10 && num < 100){
         centenas = 0;
-        decenas =  valor2Digitos();
-        unidades = valor2Digitos_unidades();
-        void numeroAPalabras();
+        decenas =  valor2Digitos(num);
+        unidades = valor2Digitos_unidades(num);
+        void numeroAPalabras(centenas, decenas, unidades);
     }
     else if(num >= 0 && num < 10){
         centenas = 0;
         decenas = 0;
         unidades = num;
-        void numeroAPalabras();
+        void numeroAPalabras(centenas, decenas, unidades);
     }
 
     return 0;
 }
 
+//With this part of the code, we're going to calculate the values for number with 3 digits...
 int valor3Digitos_centenas(int value){
     int centenas;
 
@@ -63,4 +67,26 @@ int valor3Digitos_unidades(int value){
     int unidades;
 
     unidades = value % 10;
+}
+
+//This other block of the code is for numbers with 2 digits...
+int valor2Digitos_decenas(int value){
+    int decenas;
+
+    decenas = value / 10;
+
+    return decenas;
+}
+
+int valor2Digitos_unidades(int value){
+    int unidades;
+
+    unidades = value % 10;
+
+    return unidades;
+}
+
+void numeroAPalabras(){
+
+
 }
