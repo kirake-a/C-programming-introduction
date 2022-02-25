8547120a256a70d1144fa7250b8eecd294094147
kirake-a <ruben_aalvarado@outlook.com>
2022-02-24 23:42:24 -0600

    added a new folder for the ADA2, and the first project

---
+++ EjercicioClase/ADA2/1Ejercicio.c
@@ -0,0 +1,15 @@
+/* Sin usar biblioteca de funciones (a excepción de stdio.h), escriba un programa que defina y pruebe las siguientes funciones:
+
+    Función para calcular la multiplicación de dos números, utilizando la estrategia de repetición de sumas.
+    Hacer la función que calcule el número de Fibonacci dado, el valor n que representa su posición en la serie de números de Fibonacci.
+    Hacer la función factorial.
+    Definir la función potencia.
+
+Nota: no es necesario pedir los datos al usuario, se pueden invocar las funciones con valores predefinidos. */
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
