d27df8ae38c6f05fac3a0f1b8002e32a14f89296
kirake-a <ruben_aalvarado@outlook.com>
2022-03-30 00:14:27 -0600

    Se agrego a ADA3, 2Ej la funcionalidad de conocer la calificacion menor y la mayor entre los alumnos

--- EjercicioClase/ADA3/2Ejercicio.c
+++ EjercicioClase/ADA3/2Ejercicio.c
@@ -3,6 +3,9 @@
 #define fila 3
 #define columna 5
 
+void valorCalificacionesMenores(float matriz[fila][columna]);
+void valorCalificacionesMayores(float matriz[fila][columna]);
+
 int main(){
 
     float matriz[fila][columna];
@@ -21,10 +24,51 @@ int main(){
     // Printeando la matriz obtenida de la digitacion del usuario
     for (i = 0; i < fila; i++){
         for (j = 0; j < columna; j++){
+            printf("Calif%d=\t", j+1);
             printf("%.3f\t", matriz[i][j]);
         }
         printf("\n");
     }
 
+    valorCalificacionesMayores(matriz);
+    valorCalificacionesMenores(matriz);
+
     return 0;
+}
+
+//Funcion para la evaluacion de la calificacion menor de los alumnos definidos
+void valorCalificacionesMenores(float matriz[fila][columna]){
+    float menor_calif;
+    int i, j;
+
+    menor_calif = matriz[0][0];
+
+    for (i = 0; i < fila; i++){
+        for (j = 0; j < columna; j++){
+
+            if(matriz[i][j] < menor_calif){
+                menor_calif = matriz[i][j];
+            }
+        }
+    }
+
+    printf("\nLa calificacion menor entre los alumnos es: %.3f", menor_calif);
+}
+
+void valorCalificacionesMayores(float matriz[fila][columna]){
+    float mayor_calif;
+    int i, j;
+
+    mayor_calif = matriz[0][0];
+
+    for (i = 0; i < fila; i++){
+        for (j = 0; j < columna; j++){
+
+            if(matriz[i][j] > mayor_calif){
+                mayor_calif = matriz[i][j];
+            }
+        }
+    }
+
+    printf("\nLa calificacion mayor entre los alumnos es: %.3f", mayor_calif);
 }
