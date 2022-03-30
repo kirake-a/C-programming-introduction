252986fd8b49768563cbb92447f1636a431bd0c0
kirake-a <ruben_aalvarado@outlook.com>
2022-03-30 00:43:38 -0600

    Se agrego en funcion el printeo de la matriz con la entrada de datos

--- EjercicioClase/ADA3/2Ejercicio.c
+++ EjercicioClase/ADA3/2Ejercicio.c
@@ -3,6 +3,7 @@
 #define fila 3
 #define columna 5
 
+void outputPrintMatriz(float matriz[fila][columna]);
 void valorCalificacionesMenores(float matriz[fila][columna]);
 void valorCalificacionesMayores(float matriz[fila][columna]);
 
@@ -21,54 +22,73 @@ int main(){
 
     printf("\n\n");
 
-    // Printeando la matriz obtenida de la digitacion del usuario
-    for (i = 0; i < fila; i++){
-        for (j = 0; j < columna; j++){
-            printf("Calif%d=\t", j+1);
-            printf("%.3f\t", matriz[i][j]);
-        }
-        printf("\n");
-    }
+    //Printenado la matriz resultante con los valores en entrada de datos
+    outputPrintMatriz(matriz);
 
+    //Llamando funciones para encontrar al valor mayor y menor
     valorCalificacionesMayores(matriz);
     valorCalificacionesMenores(matriz);
 
     return 0;
 }
 
-//Funcion para la evaluacion de la calificacion menor de los alumnos definidos
+// Funcion que imprime a la matriz definida por el usuario
+void outputPrintMatriz(float matriz[fila][columna]){
+    int i, j;
+
+    for (i = 0; i < fila; i++){
+        for (j = 0; j < columna; j++){
+            printf("Calif%d=\t", j + 1);
+            printf("%.3f\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Funcion para la evaluacion de la calificacion menor de los alumnos definidos
 void valorCalificacionesMenores(float matriz[fila][columna]){
     float menor_calif;
-    int i, j;
+    int i, j, alumno = 0;
 
     menor_calif = matriz[0][0];
 
+    // Creando comparaciones entre los valores de la matriz para conocer al valor menor
     for (i = 0; i < fila; i++){
         for (j = 0; j < columna; j++){
 
             if(matriz[i][j] < menor_calif){
                 menor_calif = matriz[i][j];
+
+                alumno = i + 1;
             }
         }
     }
 
-    printf("\nLa calificacion menor entre los alumnos es: %.3f", menor_calif);
+    printf("\n\nLa calificacion menor entre los alumnos es: %.3f", menor_calif);
+    // Falta conocer a que alumno le corresponde esa calif
+    printf("\nLa calificacion menor corresponde al alumno: %d", alumno);
 }
 
+// Evaluando para conocer la calificacion mayor de los alumnos definidos
 void valorCalificacionesMayores(float matriz[fila][columna]){
     float mayor_calif;
-    int i, j;
+    int i, j, alumno = 0;
 
     mayor_calif = matriz[0][0];
 
+    // Creando comparaciones entre los valores de la matriz para conocer al valor mayor...
     for (i = 0; i < fila; i++){
         for (j = 0; j < columna; j++){
 
             if(matriz[i][j] > mayor_calif){
                 mayor_calif = matriz[i][j];
+
+                alumno = i + 1;
             }
         }
     }
 
     printf("\nLa calificacion mayor entre los alumnos es: %.3f", mayor_calif);
+    // Falta conocer a que alumno le corresponde esa calif
+    printf("\nLa calificacion mayor corresponde al alumno: %d", alumno);
 }
